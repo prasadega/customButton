be35128337df5bd58be27d47015e94df1515b9b6
prasadega <140684871+prasadega@users.noreply.github.com>
2023-07-27 10:42:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let customButtonElement = document.getElementById("customButton");
+let applyButtonElement = document.getElementById("applyButton")
+
+let bgColorInputElement = document.getElementById("bgColorInput");
+let fontColorInputElement = document.getElementById("fontColorInput");
+let fontSizeInputElement = document.getElementById("fontSizeInput");
+let fontWeightInputElement = document.getElementById("fontWeightInput");
+let paddingInputElement = document.getElementById("paddingInput");
+let borderRadiusInputElement = document.getElementById("borderRadiusInput");
+
+applyButtonElement.onclick = function(){
+    let bgColorInputValue = bgColorInputElement.value;
+    let fontColorInputValue = fontColorInputElement.value;
+    let fontSizeInputValue = fontSizeInputElement.value;
+    let fontWeightInputValue = fontWeightInputElement.value;
+    let paddingInputValue = paddingInputElement.value;
+    let borderRadiusInputValue = borderRadiusInputElement.value;
+
+
+    customButtonElement.style.backgroundColor = bgColorInputValue;
+    customButtonElement.style.color = fontColorInputValue;
+    customButtonElement.style.fontSize = fontSizeInputValue;
+    customButtonElement.style.fontWeight = fontWeightInputValue;
+    customButtonElement.style.padding = paddingInputValue;
+    customButtonElement.style.borderRadius = borderRadiusInputValue;
+}
